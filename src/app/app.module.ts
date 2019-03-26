8edc0a5ff85a77da72ac6da8c1a5938bb4dcdad7
Agnimitra Das <agnimitradas@outlook.com>
2019-03-26 12:23:02 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { tutorialReducer } from "./reducers/tutorial.reducer";
+import { ReadComponent } from "./read/read.component";
+import { CreateComponent } from "./create/create.component";
+
+@NgModule({
+  declarations: [AppComponent, ReadComponent, CreateComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      tutorial1: tutorialReducer
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
